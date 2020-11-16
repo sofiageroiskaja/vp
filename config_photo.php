3ce645028ed364c7410f60e88ef8649225738e7f
Sofia Geroiskaja <sofiageroiskaja@users.noreply.github.com>
2020-11-16 21:20:11 +0200

    10 tund loppes

---
+++ config_photo.php
@@ -0,0 +1,10 @@
+<?php
+  $filesizelimit = 2097152; //1048576;
+  $photouploaddir_orig = "../photoupload_orig/";
+  $photouploaddir_normal = "../photoupload_normal/";
+  $photouploaddir_thumb = "../photoupload_thumb/";
+  $watermark = "../img/vp_logo_w100_overlay.png";
+  $photomaxwidth = 600;
+  $photomaxheight = 400;
+  $thumbsize = 100;
+  $photoFileTypes = ["image/jpeg", "image/png", "image/gif"];
